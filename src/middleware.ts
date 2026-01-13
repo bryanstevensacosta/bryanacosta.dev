f4e5029f8c29172565b9170cc416526ad64fae14
cryptoganster <ganstercrypto@gmail.com>
2026-01-13 15:54:32 -0400

    feat(i18n): add browser language detection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,15 @@
 import createMiddleware from 'next-intl/middleware'
 import { routing } from './i18n/routing'
+import { locales, defaultLocale } from './i18n/config'
+import type { NextRequest } from 'next/server'
 
-export default createMiddleware(routing)
+export default createMiddleware({
+  ...routing,
+  localeDetection: true,
+  localePrefix: routing.localePrefix,
+  defaultLocale,
+  locales,
+})
 
 export const config = {
   // Match all pathnames except for
